45f2f4f85d886add049b52646cde44ed5a78acf1
P01son6415 <liyudong123@hotmail.com>
2023-03-24 17:03:07 +0800

    add llama config

--- scripts/preprocess_coord.py
+++ scripts/preprocess_coord.py
@@ -75,7 +75,8 @@ def box_to_formular_data(keypoints):
     return output
 
 num2char = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h', 8: 'i', 9: 'j',
-            10: 'k', 11: 'l', 12: 'm', 13: 'n', 14: 'o', 15: 'p', 16: 'q', 17: 'r' }
+            10: 'k', 11: 'l', 12: 'm', 13: 'n', 14: 'o', 15: 'p', 16: 'q', 17: 'r', 18: 's', 19: 't',
+            20: 'u', 21: 'v', 22: 'w'}
 
 def formular_data_to_str(data_list, type):
 
