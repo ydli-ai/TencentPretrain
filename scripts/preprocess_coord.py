270c9fc72069e23b368c7d7140a24ceed3876ee1
P01son6415 <liyudong123@hotmail.com>
2023-03-24 16:26:34 +0800

    add llama config

--- scripts/preprocess_coord.py
+++ scripts/preprocess_coord.py
@@ -39,6 +39,41 @@ def keypoint_to_formular_data(keypoints):
     return output
 
 
+def mask_to_formular_data(keypoints):
+    output = []
+    for mask_list in keypoints:
+        output_single = {"anno_type": "mask",
+                         "prefix": "Multiple instances",
+                         "instances_num": len(mask_list),
+                         "keypoints_num": 0,
+                         "categories": [],
+                         "coordinate": []
+                         }
+        for mask in mask_list:
+            for name, point in mask.items():
+                output_single["categories"].append(name)
+                output_single["coordinate"].append(point)
+        output.append(output_single)
+    return output
+
+
+def box_to_formular_data(keypoints):
+    output = []
+    for mask_list in keypoints:
+        output_single = {"anno_type": "box",
+                         "prefix": "Multiple instances",
+                         "instances_num": len(mask_list),
+                         "keypoints_num": 0,
+                         "categories": [],
+                         "coordinate": []
+                         }
+        for mask in mask_list:
+            for name, point in mask.items():
+                output_single["categories"].append(name)
+                output_single["coordinate"].append(point)
+        output.append(output_single)
+    return output
+
 num2char = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h', 8: 'i', 9: 'j',
             10: 'k', 11: 'l', 12: 'm', 13: 'n', 14: 'o', 15: 'p', 16: 'q', 17: 'r' }
 
@@ -53,6 +88,21 @@ def formular_data_to_str(data_list, type):
             output = output + '] '
         return output
 
+    def mask_coord_to_str(keypoints):
+        output = ""
+        for points_list in keypoints:
+            output = output + '['
+            for i, point in enumerate(points_list):
+                output = output + ' ' + num2char[i] + ' $' + str(point[0][0]) + ' $'+ str(point[0][0])
+            output = output + '] '
+        return output
+
+    def box_coord_to_str(boxes):
+        output = ""
+        for box in boxes:
+            output = output + '[  xmin $' + str(box[0]) + ' ymin $'+ str(box[1]) +
+            ' ymax $'+ str(box[2]) + ' ymax $'+ str(box[3]) +'] '
+        return output
 
     output = []
     for data in data_list:
@@ -60,6 +110,10 @@ def formular_data_to_str(data_list, type):
         output_single = output_single + '; ' + ', '.join(data["categories"]) +'; '
         if type == "keypoint":
             output_single = output_single + keyporint_coord_to_str(data["coordinate"])
+        elif type == "box":
+            output_single = output_single + box_coord_to_str(data["coordinate"])
+        else:
+            output_single = output_single + mask_coord_to_str(data["coordinate"])
         output.append(output_single)
 
     return output
@@ -77,6 +131,17 @@ def main():
 
     if args.data_type == "keypoint":
         keypoints = filter_keypoint(data['keypoints'] )
+        data_json = keypoint_to_formular_data(keypoints)
+    elif args.data_type == "box":
+        data_json = box_to_formular_data(data['bboxes'])
+    else:
+        data_json = mask_to_formular_data(data['masks'])
+
+    data_str = formular_data_to_str(data_json, args.data_type)
+
+    with open(args.output_path, 'w') as f:
+        for l in data_str:
+            f.write(l + '\n')
 
 
 if __name__ == "__main__":
