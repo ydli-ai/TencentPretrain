352b66db6650ee36151e42361ecfe0e2b5f8132a
P01son6415 <liyudong123@hotmail.com>
2023-03-25 16:05:12 +0800

    add llama config

--- scripts/generate_lm.py
+++ scripts/generate_lm.py
@@ -80,7 +80,7 @@ if __name__ == '__main__':
     args = load_hyperparam(args)
 
     args.tokenizer = str2tokenizer[args.tokenizer](args)
-    print('UNK', args.tokenizer.convert_tokens_to_ids([CLS_TOKEN]))
+    print('UNK', args.tokenizer.convert_tokens_to_ids([UNK_TOKEN]))
 
     model = GenerateLm(args)
     model = load_model(model, args.load_model_path, True)
