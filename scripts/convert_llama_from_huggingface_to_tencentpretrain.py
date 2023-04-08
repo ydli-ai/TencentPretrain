216a418f01f4e30176bef38daefc80a1f9d6e8e1
P01son6415 <liyudong123@hotmail.com>
2023-04-08 13:46:42 +0800

    add llama config

--- scripts/convert_llama_from_huggingface_to_tencentpretrain.py
+++ scripts/convert_llama_from_huggingface_to_tencentpretrain.py
@@ -28,12 +28,13 @@ output_model = collections.OrderedDict()
 def get_weight_from_name(layer_name):
     return input_models[weight_map[layer_name]][layer_name]
 
+output_model["embedding.word.embedding.weight"] = get_weight_from_name("model.embed_tokens.weight")
+
 dim = output_model["embedding.word.embedding.weight"].size(1)
 
 def unpermute(w):
     return w.transpose(2, 1).view(dim, dim)
 
-output_model["embedding.word.embedding.weight"] = get_weight_from_name("model.embed_tokens.weight")
 
 for i in range(args.layers_num):
 
