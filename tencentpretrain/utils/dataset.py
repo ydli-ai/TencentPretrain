657ac6226d9056af84f51e8996fde8d1ad88b46b
P01son6415 <liyudong123@hotmail.com>
2023-04-16 11:05:29 +0800

    update

--- tencentpretrain/utils/dataset.py
+++ tencentpretrain/utils/dataset.py
@@ -991,6 +991,7 @@ class AlpacaDataset(Dataset):
                 input = data.get("input", "").replace('\\n', '\n')
                 output = data.get("output", "").replace('\\n', '\n')
                 line_text = instruction + input + output
+                print(line_text)
                 document = self.tokenizer.convert_tokens_to_ids(self.tokenizer.tokenize(line_text))
                 document = [self.vocab.get(CLS_TOKEN)] + document + [self.vocab.get(SEP_TOKEN)]
 
