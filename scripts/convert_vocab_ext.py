8e0a693439dbed0e7f8033b7ee2b03670a0d80ce
P01son6415 <liyudong123@hotmail.com>
2023-05-30 12:38:11 +0800

    update

--- scripts/convert_vocab_ext.py
+++ scripts/convert_vocab_ext.py
@@ -15,8 +15,8 @@ input_model = torch.load(args.input_model_path, map_location="cpu")
 
 print(input_model["embedding.word.embedding.weight"].size())
 print(input_model["target.lm.output_layer.weight"].size())
-input_model["embedding.word.embedding.weight"] = torch.cat(input_model["embedding.word.embedding.weight"], torch.rand(16242, 5120))
-input_model["target.lm.output_layer.weight"] = torch.cat(input_model["target.lm.output_layer.weight"], torch.rand(16242, 5120))
+input_model["embedding.word.embedding.weight"] = torch.cat([input_model["embedding.word.embedding.weight"], torch.rand(16242, 5120)], dim=0)
+input_model["target.lm.output_layer.weight"] = torch.cat([input_model["target.lm.output_layer.weight"], torch.rand(16242, 5120)], dim=0)
 
 
 torch.save(input_model, args.output_model_path)
