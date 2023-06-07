c03557fde691a14ac171f84a36ed0003ce371505
P01son6415 <liyudong123@hotmail.com>
2023-06-07 17:08:18 +0800

    update

--- scripts/convert_clip_from_huggingface_to_tencentpretrain.py
+++ scripts/convert_clip_from_huggingface_to_tencentpretrain.py
@@ -27,24 +27,63 @@ def convert_clip_transformer(input_model, output_model, layers_num):
             input_model["text_model.encoder.layers." + str(i) + ".self_attn.out_proj.bias"]
 
         output_model["encoder.encoder_0.transformer." + str(i) + ".layer_norm_1.gamma"] = \
-            input_model["text_model.encoder.layer." + str(i) + ".layer_norm1.weight"]
+            input_model["text_model.encoder.layers." + str(i) + ".layer_norm1.weight"]
         output_model["encoder.encoder_0.transformer." + str(i) + ".layer_norm_1.beta"] = \
-            input_model["text_model.encoder.layer." + str(i) + ".layer_norm1.bias"]
+            input_model["text_model.encoder.layers." + str(i) + ".layer_norm1.bias"]
 
         output_model["encoder.encoder_0.transformer." + str(i) + ".feed_forward.linear_1.weight"] = \
-            input_model["text_model.encoder.layer." + str(i) + ".mlp.fc1.weight"]
+            input_model["text_model.encoder.layers." + str(i) + ".mlp.fc1.weight"]
         output_model["encoder.encoder_0.transformer." + str(i) + ".feed_forward.linear_1.bias"] = \
-            input_model["text_model.encoder.layer." + str(i) + ".mlp.fc1.bias"]
+            input_model["text_model.encoder.layers." + str(i) + ".mlp.fc1.bias"]
         output_model["encoder.encoder_0.transformer." + str(i) + ".feed_forward.linear_2.weight"] = \
-            input_model["text_model.encoder.layer." + str(i) + ".mlp.fc2.weight"]
+            input_model["text_model.encoder.layers." + str(i) + ".mlp.fc2.weight"]
         output_model["encoder.encoder_0.transformer." + str(i) + ".feed_forward.linear_2.bias"] = \
-            input_model["text_model.encoder.layer." + str(i) + ".mlp.fc2.bias"]
+            input_model["text_model.encoder.layers." + str(i) + ".mlp.fc2.bias"]
 
-        output_model["encoder.transformer." + str(i) + ".layer_norm_2.gamma"] = \
-            input_model["bert.encoder.layer." + str(i) + ".layer_norm2.weight"]
-        output_model["encoder.transformer." + str(i) + ".layer_norm_2.beta"] = \
-            input_model["bert.encoder.layer." + str(i) + ".layer_norm2.bias"]
+        output_model["encoder.encoder_0.transformer." + str(i) + ".layer_norm_2.gamma"] = \
+            input_model["text_model.encoder.layers." + str(i) + ".layer_norm2.weight"]
+        output_model["encoder.encoder_0.transformer." + str(i) + ".layer_norm_2.beta"] = \
+            input_model["text_model.encoder.layers." + str(i) + ".layer_norm2.bias"]
 
+
+        output_model["encoder.encoder_1.transformer." + str(i) + ".self_attn.linear_layers.0.weight"] = \
+            input_model["vision_model.encoder.layers." + str(i) + ".self_attn.q_proj.weight"]
+        output_model["encoder.encoder_1.transformer." + str(i) + ".self_attn.linear_layers.0.bias"] = \
+            input_model["vision_model.encoder.layers." + str(i) + ".self_attn.q_proj.bias"]
+
+        output_model["encoder.encoder_1.transformer." + str(i) + ".self_attn.linear_layers.1.weight"] = \
+            input_model["vision_model.encoder.layers." + str(i) + ".self_attn.k_proj.weight"]
+        output_model["encoder.encoder_1.transformer." + str(i) + ".self_attn.linear_layers.1.bias"] = \
+            input_model["vision_model.encoder.layers." + str(i) + ".self_attn.k_proj.bias"]
+
+        output_model["encoder.encoder_1.transformer." + str(i) + ".self_attn.linear_layers.2.weight"] = \
+            input_model["vision_model.encoder.layers." + str(i) + ".self_attn.v_proj.weight"]
+        output_model["encoder.encoder_1.transformer." + str(i) + ".self_attn.linear_layers.2.bias"] = \
+            input_model["vision_model.encoder.layers." + str(i) + ".self_attn.v_proj.bias"]
+
+        output_model["encoder.encoder_1.transformer." + str(i) + ".self_attn.final_linear.weight"] = \
+            input_model["vision_model.encoder.layers." + str(i) + ".self_attn.out_proj.weight"]
+        output_model["encoder.encoder_1.transformer." + str(i) + ".self_attn.final_linear.weight"] = \
+            input_model["vision_model.encoder.layers." + str(i) + ".self_attn.out_proj.bias"]
+
+        output_model["encoder.encoder_1.transformer." + str(i) + ".layer_norm_1.gamma"] = \
+            input_model["vision_model.encoder.layers." + str(i) + ".layer_norm1.weight"]
+        output_model["encoder.encoder_1.transformer." + str(i) + ".layer_norm_1.beta"] = \
+            input_model["vision_model.encoder.layers." + str(i) + ".layer_norm1.bias"]
+
+        output_model["encoder.encoder_1.transformer." + str(i) + ".feed_forward.linear_1.weight"] = \
+            input_model["vision_model.encoder.layers." + str(i) + ".mlp.fc1.weight"]
+        output_model["encoder.encoder_1.transformer." + str(i) + ".feed_forward.linear_1.bias"] = \
+            input_model["vision_model.encoder.layers." + str(i) + ".mlp.fc1.bias"]
+        output_model["encoder.encoder_1.transformer." + str(i) + ".feed_forward.linear_2.weight"] = \
+            input_model["vision_model.encoder.layers." + str(i) + ".mlp.fc2.weight"]
+        output_model["encoder.encoder_1.transformer." + str(i) + ".feed_forward.linear_2.bias"] = \
+            input_model["vision_model.encoder.layers." + str(i) + ".mlp.fc2.bias"]
+
+        output_model["encoder.encoder_1.transformer." + str(i) + ".layer_norm_2.gamma"] = \
+            input_model["vision_model.encoder.layers." + str(i) + ".layer_norm2.weight"]
+        output_model["encoder.encoder_1.transformer." + str(i) + ".layer_norm_2.beta"] = \
+            input_model["vision_model.encoder.layers." + str(i) + ".layer_norm2.bias"]
 
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
