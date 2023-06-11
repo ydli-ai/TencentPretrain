e560b1d0bdee79686b921f064918578d621df0e4
P01son6415 <liyudong123@hotmail.com>
2023-06-11 17:07:57 +0800

    update

--- tencentpretrain/trainer.py
+++ tencentpretrain/trainer.py
@@ -598,6 +598,7 @@ def worker(proc_id, gpu_ranks, args, model_for_training, model_for_dataloader=No
             {"params": [p for n, p in param_optimizer if 'embedding' in n or "output_layer" in n], "lr": args.learning_rate},
             {"params": [p for n, p in param_optimizer if 'embedding' not in n or "output_layer" not in n], "lr": 0}
         ]
+        print(optimizer_grouped_parameters)
         #for n, p in list(model_for_training.named_parameters()):
         #    if 'embedding' not in n or "output_layer" not in n:
         #        p.requires_grad = False
