eda98dd9c2461ff3e4e1092eff3ea7157b3ed648
P01son6415 <liyudong123@hotmail.com>
2023-06-13 16:38:59 +0800

    update

--- scripts/convert_falcon_from_tencentpretrain_to_huggingface.py
+++ scripts/convert_falcon_from_tencentpretrain_to_huggingface.py
@@ -18,19 +18,19 @@ args = parser.parse_args()
 input_model = torch.load(args.input_model_path)
 output_model = collections.OrderedDict()
 
-output_model["transformer.word_embeddings.weight"] = input_model["embedding.word.embedding.weight"].bfloat16()
+output_model["transformer.word_embeddings.weight"] = input_model["embedding.word.embedding.weight"]
 
 for i in range(args.layers_num):
 
-    output_model["transformer.h." + str(i) + ".input_layernorm.weight"] = input_model["encoder.transformer." + str(i) + ".layer_norm_1.weight"].bfloat16()
-    output_model["transformer.h." + str(i) + ".input_layernorm.bias"] = input_model["encoder.transformer." + str(i) + ".layer_norm_1.bias"].bfloat16()
-    output_model["transformer.h." + str(i) + ".self_attention.query_key_value.weight"] = input_model["encoder.transformer." + str(i) + ".self_attn.query_key_value.weight"].bfloat16()
-    output_model["transformer.h." + str(i) + ".self_attention.dense.weight"] = input_model["encoder.transformer." + str(i) + ".self_attn.dense.weight"].bfloat16()
-    output_model["transformer.h." + str(i) + ".mlp.dense_h_to_4h.weight"] = input_model["encoder.transformer." + str(i) + ".feed_forward.linear_1.weight"].bfloat16()
-    output_model["transformer.h." + str(i) + ".mlp.dense_4h_to_h.weight"] = input_model["encoder.transformer." + str(i) + ".feed_forward.linear_2.weight"].bfloat16()
+    output_model["transformer.h." + str(i) + ".input_layernorm.weight"] = input_model["encoder.transformer." + str(i) + ".layer_norm_1.weight"]
+    output_model["transformer.h." + str(i) + ".input_layernorm.bias"] = input_model["encoder.transformer." + str(i) + ".layer_norm_1.bias"]
+    output_model["transformer.h." + str(i) + ".self_attention.query_key_value.weight"] = input_model["encoder.transformer." + str(i) + ".self_attn.query_key_value.weight"]
+    output_model["transformer.h." + str(i) + ".self_attention.dense.weight"] = input_model["encoder.transformer." + str(i) + ".self_attn.dense.weight"]
+    output_model["transformer.h." + str(i) + ".mlp.dense_h_to_4h.weight"] = input_model["encoder.transformer." + str(i) + ".feed_forward.linear_1.weight"]
+    output_model["transformer.h." + str(i) + ".mlp.dense_4h_to_h.weight"] = input_model["encoder.transformer." + str(i) + ".feed_forward.linear_2.weight"]
 
-output_model["transformer.ln_f.weight"] = input_model["encoder.layer_norm.weight"].bfloat16()
-output_model["transformer.ln_f.bias"] = input_model["encoder.layer_norm.bias"].bfloat16()
-output_model["lm_head.weight"] = input_model["target.lm.output_layer.weight"].bfloat16()
+output_model["transformer.ln_f.weight"] = input_model["encoder.layer_norm.weight"]
+output_model["transformer.ln_f.bias"] = input_model["encoder.layer_norm.bias"]
+output_model["lm_head.weight"] = input_model["target.lm.output_layer.weight"]
 
 torch.save(output_model, args.output_model_path)
