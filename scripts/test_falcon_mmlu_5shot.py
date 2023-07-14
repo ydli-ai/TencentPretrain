68d1184bf12694f6c2bc44ee3d84876c57bc9c3d
P01son6415 <liyudong123@hotmail.com>
2023-07-14 15:38:38 +0800

    update

--- scripts/test_falcon_mmlu_5shot.py
+++ scripts/test_falcon_mmlu_5shot.py
@@ -135,6 +135,7 @@ if __name__ == '__main__':
         for file in os.listdir('../mmlu/data/val'):
             fw.write(file + '\t')
             questions = []
+            test_file = "_".join(file.split('_')[:-1]) + '_test.csv'
             df = pd.read_csv('../mmlu/data/val/'+file, names=["question", "A", "B", "C", "D", "answer"], header=None)
             prefix_list = []
             for index, row in df.iterrows():
@@ -147,7 +148,7 @@ if __name__ == '__main__':
 
                 prefix_list.append(prefix)
 
-            df = pd.read_csv('../mmlu/data/test/'+file, names=["question", "A", "B", "C", "D", "answer"], header=None)
+            df = pd.read_csv('../mmlu/data/test/'+test_file, names=["question", "A", "B", "C", "D", "answer"], header=None)
             for index, row in df.iterrows():
 
                 question = "Question: " + row['question']
