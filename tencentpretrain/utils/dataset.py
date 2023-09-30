de334decb95db1131749a232455723ebc1b9f7b6
P01son6415 <liyudong123@hotmail.com>
2023-09-30 23:50:49 +0800

    update

--- tencentpretrain/utils/dataset.py
+++ tencentpretrain/utils/dataset.py
@@ -465,9 +465,7 @@ class LmDataset(Dataset):
                         #instruction = data.get("instruction", "").replace('\\n', '\n')
                         #input = data.get("input", "").replace('\\n', '\n')
                         #output = data.get("output", "").replace('\\n', '\n')
-                        #if data.get('score', None) is not None:
-                        #    if data['score'][1] > 0.9:
-                        #        continue
+
                     except:
                         continue
                     if len(text) > 0:
@@ -476,7 +474,9 @@ class LmDataset(Dataset):
                         line = content
                     else:
                         continue
-
+                    if data.get('score', None) is not None:
+                        if data['score']['1'] > 0.9:
+                            continue
                     if len(title) > 0:
                         line = title + '\n' + line
 
